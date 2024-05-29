8736a01f52d67df7167e014155878215ce8442a9
Martin <pukiyolo@gmail.com>
2024-05-29 23:47:46 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function App() {
 	const router = useRouter();
 	const { register, handleSubmit } = useForm<FormValues>();
 	const [todos, setTodos] = useState<any[]>([]); // Add type annotation for todos
+	const [loading, setLoading] = useState<boolean>(true);
 
 	const onSubmit = (data: FormValues) => {
 		fetch(`https://6653697c1c6af63f4674a111.mockapi.io/api/users/${getWithExpiry("user")}/todoLists`, {
@@ -43,63 +44,72 @@ export default function App() {
 			.then((response) => response.json())
 			.then((responseData) => {
 				setTodos(responseData);
+				setLoading(false);
 			})
 			.catch((error) => console.error(error));
-	}, [todos]);
+	}, [todos, loading]);
 
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div style={{ width: "100%", padding: "20px", display: "flex", alignItems: "end", flexDirection: "column" }}>
-				<button
-					className="btn btn-primary"
-					onClick={() => {
-						localStorage.removeItem("user");
-						router.push("/login");
-					}}
-				>
-					Logout
-				</button>
-				<button
-					className="btn btn-primary"
-					onClick={() => (document.getElementById("my_modal") as HTMLDialogElement)?.showModal()}
-				>
-					+
-				</button>
+	if (loading === true) {
+		return (
+			<div className="flex justify-center items-center h-screen w-full">
+				<span className="loading loading-spinner w-12"></span>
 			</div>
-			{todos.map(({ id, createdAt, title }) => (
-				<div key={"key" + id}>
-					<Link className="btn btn-primary" href={`/todo-list/${id}`}>
-						<p>{createdAt}</p>
-						<p>{title}</p>
-					</Link>
-					<button className="btn btn-error" onClick={() => deleteTodo(id)}>
-						Delete
+		);
+	} else {
+		return (
+			<main className="flex min-h-screen flex-col items-center justify-between p-24">
+				<div className="flex flex-col items-end w-full" style={{ padding: "20px" }}>
+					<button
+						className="btn btn-primary"
+						onClick={() => {
+							localStorage.removeItem("user");
+							router.push("/login");
+						}}
+					>
+						Logout
+					</button>
+					<button
+						className="btn btn-primary"
+						onClick={() => (document.getElementById("my_modal") as HTMLDialogElement)?.showModal()}
+					>
+						+
 					</button>
 				</div>
-			))}
-			<dialog id="my_modal" className="modal">
-				<div className="modal-box">
-					<form method="dialog">
-						<button className="btn btn-sm btn-circle btn-ghost absolute right-4 top-4">✕</button>
+				{todos.map(({ id, createdAt, title }) => (
+					<div key={"key" + id}>
+						<Link className="btn btn-primary" href={`/todo-list/${id}`}>
+							<p>{createdAt}</p>
+							<p>{title}</p>
+						</Link>
+						<button className="btn btn-error" onClick={() => deleteTodo(id)}>
+							Delete
+						</button>
+					</div>
+				))}
+				<dialog id="my_modal" className="modal">
+					<div className="modal-box">
+						<form method="dialog">
+							<button className="btn btn-sm btn-circle btn-ghost absolute right-4 top-4">✕</button>
+						</form>
+						<h3 className="font-bold text-lg mb-3">New todo list</h3>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<input {...register("title")} className="input input-bordered w-full" type="text" placeholder="Title" />
+							<div className="modal-action">
+								<button
+									type="submit"
+									className="btn"
+									onClick={() => (document.getElementById("my_modal") as HTMLDialogElement)?.close()}
+								>
+									Add
+								</button>
+							</div>
+						</form>
+					</div>
+					<form method="dialog" className="modal-backdrop">
+						<button>close</button>
 					</form>
-					<h3 className="font-bold text-lg mb-3">New todo list</h3>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<input {...register("title")} className="input input-bordered w-full" type="text" placeholder="Title" />
-						<div className="modal-action">
-							<button
-								type="submit"
-								className="btn"
-								onClick={() => (document.getElementById("my_modal") as HTMLDialogElement)?.close()}
-							>
-								Add
-							</button>
-						</div>
-					</form>
-				</div>
-				<form method="dialog" className="modal-backdrop">
-					<button>close</button>
-				</form>
-			</dialog>
-		</main>
-	);
+				</dialog>
+			</main>
+		);
+	}
 }
--- src/app/todo-list/[todolist]/page.tsx
+++ src/app/todo-list/[todolist]/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import { getWithExpiry } from "@/app/components/functions";
 
 type FormValues = {
@@ -17,6 +17,7 @@ export default function Todolist({ params }: { params: { todolist: string } }) {
 	const router = useRouter();
 	const { register, handleSubmit } = useForm<FormValues>();
 	const [todoItems, setTodoItems] = useState<any[]>([]); // Add type annotation for todos
+	const [loading, setLoading] = useState<boolean>(true);
 
 	const onSubmit = (data: FormValues) => {
 		const date = new Date();
@@ -36,6 +37,7 @@ export default function Todolist({ params }: { params: { todolist: string } }) {
 			}
 		)
 			.then((response) => response.json())
+			.then(() => setLoading(true))
 			.catch((error) => console.error(error));
 	};
 
@@ -54,6 +56,7 @@ export default function Todolist({ params }: { params: { todolist: string } }) {
 			}
 		)
 			.then((response) => response.json())
+			.then(() => setLoading(true))
 			.catch((error) => console.error(error));
 	};
 
@@ -67,6 +70,7 @@ export default function Todolist({ params }: { params: { todolist: string } }) {
 			}
 		)
 			.then((response) => response.json())
+			.then(() => setLoading(true))
 			.catch((error) => console.error(error));
 	};
 
@@ -82,115 +86,123 @@ export default function Todolist({ params }: { params: { todolist: string } }) {
 		)
 			.then((response) => response.json())
 			.then((responseData) => {
+				for (let i = 0; i < responseData.length; i++) {
+					if (responseData[i].state === "1" && new Date(responseData[i].deadline).getTime() < new Date().getTime()) {
+						doneTodo(responseData[i].id, "0");
+					}
+				}
 				setTodoItems(responseData);
+				setLoading(false);
 			})
 			.catch((error) => console.error(error));
-	}, [todoItems]);
+	}, [todoItems, loading]);
 
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div style={{ width: "100%", padding: "20px", display: "flex", alignItems: "end", flexDirection: "column" }}>
-				<button
-					className="btn btn-primary"
-					onClick={() => {
-						localStorage.removeItem("user");
-						router.push("/login");
-					}}
-				>
-					Logout
-				</button>
-				<button
-					className="btn btn-primary"
-					onClick={() => (document.getElementById("my_modal") as HTMLDialogElement)?.showModal()}
-				>
-					+
-				</button>
+	if (loading === true) {
+		return (
+			<div className="flex justify-center items-center h-screen w-full">
+				<span className="loading loading-spinner w-12"></span>
 			</div>
-			<div className="text-sm breadcrumbs">
-				<ul>
-					<li>
-						<Link href="/">Home</Link>
-					</li>
-					<li>
-						<Link href="#">Todo list {params.todolist}</Link>
-					</li>
-				</ul>
-			</div>
-			<div className="flex flex-col">
-				{todoItems.map(({ id, todoListId, createdAt, title, description, deadline, state }) => (
-					<div
-						className="flex justify-between w-full"
-						key={"key" + id}
-						style={state == 0 ? { backgroundColor: "gray" } : {}}
+		);
+	} else {
+		return (
+			<main className="flex min-h-screen flex-col items-center justify-between p-24">
+				<div className="flex flex-col items-end w-full" style={{ padding: "20px" }}>
+					<button
+						className="btn btn-primary"
+						onClick={() => {
+							localStorage.removeItem("user");
+							router.push("/login");
+						}}
 					>
-						<button
-							id="doneBtn"
-							className="btn btn-outline btn-primary"
-							onClick={() => doneTodo(id, state === "0" ? "1" : "0")}
+						Logout
+					</button>
+				</div>
+				<div className="text-sm breadcrumbs">
+					<ul>
+						<li>
+							<Link href="/">Home</Link>
+						</li>
+						<li>
+							<Link href="#">Todo list {params.todolist}</Link>
+						</li>
+					</ul>
+				</div>
+				<div className="flex flex-col">
+					{todoItems.map(({ id, todoListId, createdAt, title, description, deadline, state }) => (
+						<div
+							className="flex justify-between w-full"
+							key={"key" + id}
+							style={state == 0 ? { backgroundColor: "gray" } : {}}
 						>
-							{state === "0" ? (
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="20"
-									height="20"
-									fill="white"
-									className="bi bi-check-circle-fill"
-									viewBox="0 0 16 16"
-								>
-									<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-								</svg>
-							) : (
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="20"
-									height="20"
-									fill="#7480ff"
-									className="bi bi-check-circle"
-									viewBox="0 0 16 16"
-								>
-									<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-									<path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
-								</svg>
-							)}
-						</button>
-						{/* <p>{todoListId}</p> */}
-						<p>{title}</p>
-						<p>{description}</p>
-						<p>{createdAt}</p>
-						<p>{state === "1" ? deadline : "Completed"}</p>
-						<p>{}</p>
-						<button className="btn btn-error" onClick={() => deleteTodo(id)}>
-							Delete
-						</button>
-					</div>
-				))}
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<div className="flex">
-						<input {...register("title")} className="input input-bordered w-full" type="text" placeholder="Title" />
-						<input
-							{...register("description")}
-							className="input input-bordered w-full"
-							type="text"
-							placeholder="Description"
-						/>
-						<input
-							{...register("deadline")}
-							className="input input-bordered w-full"
-							type="datetime-local"
-							placeholder="Deadline"
-						/>
-					</div>
-					<div className="modal-action">
-						<button
-							type="submit"
-							className="btn"
-							onClick={() => (document.getElementById("my_modal") as HTMLDialogElement)?.close()}
-						>
-							Add
-						</button>
-					</div>
-				</form>
-			</div>
-		</main>
-	);
+							<button
+								id="doneBtn"
+								className="btn btn-outline btn-primary"
+								onClick={
+									new Date(deadline).getTime() < new Date().getTime()
+										? () => alert("Deadline is passed")
+										: () => doneTodo(id, state === "0" ? "1" : "0")
+								}
+							>
+								{state === "0" ? (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="20"
+										height="20"
+										fill="white"
+										className="bi bi-check-circle-fill"
+										viewBox="0 0 16 16"
+									>
+										<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+									</svg>
+								) : (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="20"
+										height="20"
+										fill="#7480ff"
+										className="bi bi-check-circle"
+										viewBox="0 0 16 16"
+									>
+										<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+										<path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
+									</svg>
+								)}
+							</button>
+							{/* <p>{todoListId}</p> */}
+							<p>{title}</p>
+							<p>{description}</p>
+							<p>{createdAt}</p>
+							<p>{state === "1" ? deadline : "Completed"}</p>
+							<p>{}</p>
+							<button className="btn btn-error" onClick={() => deleteTodo(id)}>
+								Delete
+							</button>
+						</div>
+					))}
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div className="flex">
+							<input {...register("title")} className="input input-bordered w-full" type="text" placeholder="Title" />
+							<input
+								{...register("description")}
+								className="input input-bordered w-full"
+								type="text"
+								placeholder="Description"
+							/>
+							<input
+								{...register("deadline")}
+								className="input input-bordered w-full"
+								type="datetime-local"
+								placeholder="Deadline"
+							/>
+						</div>
+						<div className="modal-action">
+							<button type="submit" className="btn">
+								Add
+							</button>
+						</div>
+					</form>
+				</div>
+			</main>
+		);
+	}
 }
